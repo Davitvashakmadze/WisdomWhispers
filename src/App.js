46c1635e806110351a08b9cabde58febb84be1f9
Daviti Vashakmadze <vashaymadzedavit@gmail.com>
2024-02-07 21:55:41 +0400

    feat: add body background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import Home from './components/pages/home';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../pages/home.scss"
+const home = () => {
+  return (
+    <div className='app'>
+      
+    </div>
+  )
+}
+
+export default home
